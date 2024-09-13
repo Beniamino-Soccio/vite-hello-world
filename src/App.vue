48c99f7f04416f03ad38632927f03e59e45af790
Beniamino-Soccio <beniamino43@gmail.com>
2024-09-13 16:38:16 +0200

    added AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   components:{
-    AppTitle
+    AppTitle,
+    AppHero
   },
   data() {
     return {
@@ -14,7 +16,12 @@ export default {
 </script>
 
 <template>
-  <AppTitle />
+  <div>
+    <AppTitle />
+  </div>
+  <div>
+    <AppHero />
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+    <p>ciaooo</p>
+</template>
+
+<style scoped>
+
+</style>
